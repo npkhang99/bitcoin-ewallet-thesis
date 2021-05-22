60e9be9ef99dc567ca9dfe58980bbf6aeae130b9
Phuc-Khang Nguyen <npkhang99@gmail.com>
2021-05-22 16:40:12 +0700

    Refactor tests

---
+++ src/tests.h
@@ -0,0 +1,17 @@
+#ifndef LIBBITCOIN_THESIS_TESTS_H
+#define LIBBITCOIN_THESIS_TESTS_H
+
+#include <iostream>
+#include <string>
+#include <bitcoin/bitcoin.hpp>
+
+#include "hd_private.h"
+#include "hd_wallet.h"
+
+#include "utilities.h"
+
+void test_address();
+
+void test_hd_wallet();
+
+#endif //LIBBITCOIN_THESIS_TESTS_H

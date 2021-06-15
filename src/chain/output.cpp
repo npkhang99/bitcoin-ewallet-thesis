cc1a01bfc311f818eb511f12c0b9bd638b400370
Phuc-Khang Nguyen <npkhang99@gmail.com>
2021-06-16 03:17:34 +0700

    Refactor code

--- src/chain/output.cpp
+++ src/chain/output.cpp
@@ -8,38 +8,17 @@ void output::set_satoshi(uint64_t satoshi) {
     _satoshi = satoshi;
 }
 
-const bc::data_chunk& output::get_script_pub_key() const {
-    return _script_pub_key;
+const bc::chain::script& output::get_script_pub_key() const {
+    return _script;
 }
 
-void output::set_script_pub_key(const bc::data_chunk& script_pub_key) {
-    _script_pub_key = script_pub_key;
-}
-
-bc::data_chunk output::make_locking_script(const bc::short_hash& pubkey) {
-    bc::machine::operation::list p2kh;
-    p2kh.push_back(bc::machine::operation(bc::machine::opcode::dup));
-    p2kh.push_back(bc::machine::operation(bc::machine::opcode::hash160));
-
-    bc::machine::operation op_pubkey = bc::machine::operation(bc::to_chunk(pubkey));
-    p2kh.push_back(op_pubkey);
-
-    p2kh.push_back(bc::machine::operation(bc::machine::opcode::equalverify));
-    p2kh.push_back(bc::machine::operation(bc::machine::opcode::checksig));
-
-#ifdef DEBUG
-    bc::machine::operation::list control_p2kh =
-            bc::chain::script::to_pay_key_hash_pattern(pubkey);
-    assert(control_p2kh == p2kh);
-#endif
-
-    return bc::chain::script(p2kh).to_data(true);
+void output::set_script(const bc::chain::script& script_pub_key) {
+    _script = script_pub_key;
 }
 
 bc::data_chunk output::to_data() const {
-    bc::data_chunk output_chunk;
-    bc::extend_data(output_chunk, bc::to_little_endian(_satoshi));
-    output_chunk.push_back(_script_pub_key.size());
-    bc::extend_data(output_chunk, _script_pub_key);
-    return bc::data_chunk();
+    bc::data_chunk raw;
+    bc::extend_data(raw, bc::to_little_endian(_satoshi));
+    bc::extend_data(raw, _script.to_data(true));
+    return raw;
 }
--- src/chain/output.h
+++ src/chain/output.h
@@ -10,23 +10,15 @@ public:
 
     void set_satoshi(uint64_t satoshi);
 
-    const bc::data_chunk& get_script_pub_key() const;
+    const bc::chain::script& get_script_pub_key() const;
 
-    void set_script_pub_key(const bc::data_chunk& script_pub_key);
-
-    /**
-     * Create p2kh locking script (script pub key) with corresponding public key
-     * point hash
-     * @param pubkey the public key point hash
-     * @return list of p2kh operations
-     */
-    bc::data_chunk make_locking_script(const bc::short_hash& pubkey);
+    void set_script(const bc::chain::script& script_pub_key);
 
     bc::data_chunk to_data() const;
 
 private:
     uint64_t _satoshi;
-    bc::data_chunk _script_pub_key;
+    bc::chain::script _script;
 };
 
 

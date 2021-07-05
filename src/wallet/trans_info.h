4a8dabcce7509b77cdcdfa620c8cd695de691a99
Phuc-Khang Nguyen <npkhang99@gmail.com>
2021-07-06 01:10:05 +0700

    Add to_string method

--- src/wallet/trans_info.h
+++ src/wallet/trans_info.h
@@ -1,12 +1,20 @@
 #ifndef LIBBITCOIN_THESIS_TRANS_INFO_H
 #define LIBBITCOIN_THESIS_TRANS_INFO_H
 
+#include <string>
 #include <bitcoin/bitcoin.hpp>
 
 struct trans_info {
     hd_private priv;
     bc::chain::output_point previous_tx;
     uint64_t value;
+
+    std::string to_string() const {
+        std::string str;
+        str = "TXID: " + bc::encode_hash(previous_tx.hash()) + " | " +
+              "Value: " + bc::encode_base10(value, 8) + " BTC";
+        return str;
+    }
 };
 
 #endif //LIBBITCOIN_THESIS_TRANS_INFO_H

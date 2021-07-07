fba9eda055e871e9d11c7f9f9b812b64bb831f8e
Phuc-Khang Nguyen <npkhang99@gmail.com>
2021-07-07 12:35:04 +0700

    Make include json more flexible

--- src/chain/chain_utilities.h
+++ src/chain/chain_utilities.h
@@ -2,9 +2,13 @@
 #define LIBBITCOIN_THESIS_CHAIN_UTILITIES_H
 
 #include <bitcoin/bitcoin.hpp>
-#include <bitcoin/client.hpp>
 
-#include <json/json.h>
+#if __has_include(<json/json.h>)
+    #include <json/json.h>
+#else
+    #include <jsoncpp/json/json.h>
+#endif
+
 #include <sstream>
 
 #include "../client/http_client.h"
--- src/client/client_utilities.h
+++ src/client/client_utilities.h
@@ -4,7 +4,11 @@
 #include <string>
 #include <sstream>
 
-#include <json/json.h>
+#if __has_include(<json/json.h>)
+    #include <json/json.h>
+#else
+    #include <jsoncpp/json/json.h>
+#endif
 
 #include "http_client.h"
 
